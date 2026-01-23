299beea12b667a1cb6ae7bdeccc998e32d90f5a1
haxxZ1n <pedroguimaraes694@gmail.com>
2026-01-23 20:14:13 -0300

    atualização

--- desafios/desafio01/script.js
+++ desafios/desafio01/script.js
@@ -1,1 +1,7 @@
-alert('Olá!')
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+}
