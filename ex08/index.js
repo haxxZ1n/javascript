b5e378aa2f4e8d438b4a424a136a9d733eb9fb99
haxxZ1n <pedroguimaraes694@gmail.com>
2026-01-23 12:13:17 -0300

    atualização

---
+++ ex08/index.js
@@ -0,0 +1,6 @@
+var vel = 16
+console.log(`A velocidade o seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Diriga sempre usando cinto de segurança!')
